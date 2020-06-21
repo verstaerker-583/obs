fe38290d075b64a0d8f73ee79513c7e2536387e2
verstaerker-583 <o.koepke@gmx.de>
2020-06-21 02:16:28 +0200

    Initial commit

--- bd/html/build.js
+++ bd/html/build.js
@@ -1,5 +1,5 @@
-var w0 = window.innerWidth;
-var h0 = window.innerHeight;
+var w0 = 1280;
+var h0 = 720;
 var x0 = 0;
 var y0 = 0;
 
@@ -11,35 +11,81 @@ var y1 = (h0 - h1) / 3;
 var w2 = 2 * h1 / 3;
 var x2 = w0 - x1 - w2; 
 
-var path0 = "M0 0 h" + w0 + "v" + h0 + "h" + -w0 + "z";
+var path0 = "M0 0 h"+w0+"v"+h0+"h"+-w0+"z";
+
+function cams(svg) {
+	var elmnt = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+	elmnt.setAttribute("width",h1/3);
+	elmnt.setAttribute("height",h1/2);
+	elmnt.setAttribute("x",-h1/6);
+	elmnt.setAttribute("y",-h1/4);
+	elmnt.setAttribute("transform","translate(106,360) rotate(-2)")
+	elmnt.classList.add("frames");
+	svg.appendChild(elmnt);
+}
 
 function backgroundA(svg) {
-	var elmnt = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+	var path1 = "M"+x1+" "+y1+"v"+h1+"h"+w1+"v"+-h1+"z";
 
-	var path1 = "M" + x1 + " " + y1 + "v" + h1 + "h" + w1 + "v" + -h1 + "z";
+
 	var path = path0 + path1;
-
+	var elmnt = document.createElementNS("http://www.w3.org/2000/svg", "path");
 	elmnt.setAttribute("d",path);
 	elmnt.classList.add("background");
+	svg.appendChild(elmnt);
 
+
+	path = path1;
+	var elmnt = document.createElementNS("http://www.w3.org/2000/svg", "path");
+	elmnt.setAttribute("d",path);
+	elmnt.classList.add("frames");
 	svg.appendChild(elmnt);
 }
 
 function backgroundB(svg) {
-	var elmnt = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+	var r = 280;
+	var cx1 = 1280/4;
+	var cx2 = 1280 * 3/4;
+	var cy1 = 370;
+	var cy2 = 310;
+	var path1 = "M "+cx1+" "+cy1+" m -"+r+" 0 a "+r+" "+r+" 0 1 0 "+(r*2)+" 0 a "+r+" "+r+" 0 1 0 -"+(r*2)+" 0"; 
+	var path2 = "M "+cx2+" "+cy2+" m -"+r+" 0 a "+r+" "+r+" 0 1 0 "+(r*2)+" 0 a "+r+" "+r+" 0 1 0 -"+(r*2)+" 0"; 
 
-	var path1 = "M" + x1 + " " + y1 + "v" + h1 + "h" + w2 + "v" + -h1 + "z";
-	var path2 = "M" + x2 + " " + y1 + "v" + h1 + "h" + w2 + "v" + -h1 + "z";
 	var path = path0 + path1 + path2;
-
+	var elmnt = document.createElementNS("http://www.w3.org/2000/svg", "path");
 	elmnt.setAttribute("d",path);
 	elmnt.classList.add("background");
+	svg.appendChild(elmnt);
 
+	path = path1;
+	var elmnt = document.createElementNS("http://www.w3.org/2000/svg", "path");
+	elmnt.setAttribute("d",path);
+	elmnt.classList.add("frames");
+	elmnt.id = "path1";
+	svg.appendChild(elmnt);
+
+	path = path2;
+	var elmnt = document.createElementNS("http://www.w3.org/2000/svg", "path");
+	elmnt.setAttribute("d",path);
+	elmnt.classList.add("frames");
+	elmnt.id = "path2";
+	svg.appendChild(elmnt);
+
+	var elmnt = document.createElementNS("http://www.w3.org/2000/svg", "use");
+	elmnt.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#buntdenker");
+	svg.appendChild(elmnt);
+
+	var elmnt = document.createElementNS("http://www.w3.org/2000/svg", "use");
+	elmnt.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#Lutz");
+	svg.appendChild(elmnt);
+
+	var elmnt = document.createElementNS("http://www.w3.org/2000/svg", "use");
+	elmnt.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#Markus");
 	svg.appendChild(elmnt);
 }
 
-function build(scene) {
-	var svg = document.getElementById("container");
+function build(evt) {
+	var svg = evt.target;
 
 	var URL = location.pathname;
 	var PageName = URL.substring(URL.lastIndexOf("/") + 1);
@@ -56,6 +102,7 @@ function build(scene) {
 		break;
 	case "start":
 		backgroundA(svg);
+		cams(svg);
 		break;
 	}
 }
