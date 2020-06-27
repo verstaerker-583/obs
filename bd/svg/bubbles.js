b1e23b2e7e98a9325c327996d0adec174bd50082
verstaerker-583 <o.koepke@gmx.de>
2020-06-27 21:36:13 +0200

    Initial commit

--- bd/svg/bubbles.js
+++ bd/svg/bubbles.js
@@ -18,12 +18,14 @@ function banner() {
 	svgText("buntdenker.de", "75%", "95%").classList.add("logo");
 }
 
-function bubble(name, offset) {
+function bubble(name, offset, myClass) {
 	var path = "M0 0 m" + -r + " 0 a1 1 0 0 0 " + 2 * r + " 0 a1 1 0 0 0 " + 2 * -r + " 0";
 	var elmnt = document.createElementNS(svgNS, "path");
 	elmnt.classList.add("frames");
+	elmnt.classList.add(myClass);
 	elmnt.id = name;
 	elmnt.setAttribute("d", path);
+
 	var text = document.createElementNS(svgNS, "text");
 	text.classList.add("names");
 	text.setAttribute("dy", "1em");
@@ -33,8 +35,8 @@ function bubble(name, offset) {
 	textPath.textContent = name;
 	text.appendChild(textPath);
 	svg.appendChild(text);
+
 	svg.appendChild(elmnt);
-	return elmnt;
 }
 
 function bubbles() {
@@ -45,26 +47,28 @@ function bubbles() {
 		case "start":
 			commons();
 			svgText("Gleich geht's los!", "50%", "25%").classList.add("text");
-			useImage("#Lutz", "Lutz");
-			useImage("#Markus", "Markus");
+			useImage("Lutz", "smallImages", "Lutz");
+			useImage("Markus", "smallImages", "Markus");
 			break;
 		case "cam":
 			r = 305;
 			background();
-			bubble("Lutz Jäkel", "37.5%").classList.add("Lutz");
-			bubble("Markus Mauthe", "12.5%").classList.add("Markus");
-			useImage("#LutzBig", "Lutz");
+			bubble("Lutz Jäkel", "37.5%", "Lutz");
+			bubble("Markus Mauthe", "12.5%", "Markus");
+			useImage("Lutz", "bigImages", "Lutz");
+			useImage("Markus", "bigImages", "Markus");
 			break;
 		case "cams":
-			bubble("Lutz Jäkel", "25%").classList.add("camsLutz");
-			bubble("Markus Mauthe", "25%").classList.add("camsMarkus");
-			useImage("#Lutz", "camsLutz");
+			bubble("Lutz Jäkel", "37.5%", "camsLutz");
+			bubble("Markus Mauthe", "12.5%", "camsMarkus");
+			useImage("Lutz", "smallImages", "camsLutz");
+			useImage("Markus", "smallImages", "camsMarkus");
 			break;
 		case "finish":
 			commons();
 			svgText("Vielen Dank und auf Wiedersehen!", "50%", "25%").classList.add("text");
-			useImage("#LutzYT", "Lutz");
-			useImage("#MarkusYT", "Markus");
+			useImage("LutzYT", "smallImages", "Lutz");
+			useImage("MarkusYT", "smallImages", "Markus");
 			break;
 	}
 	banner();
@@ -94,10 +98,10 @@ function checkTime(i) {
 
 function commons() {
 	background();
-	bubble("Lutz Jäkel", "37.5%").classList.add("Lutz");
-	bubble("Markus Mauthe", "12.5%").classList.add("Markus");
+	bubble("Lutz Jäkel", "37.5%", "Lutz");
+	bubble("Markus Mauthe", "12.5%", "Markus");
 	loadClock();
-	useImage("#buntdenker", "buntdenker");
+	useImage("buntdenker", "buntdenker0", "buntdenker1");
 }
 
 function loadClock() {
@@ -141,9 +145,11 @@ function svgText(text, x, y) {
 	return elmnt;
 }
 
-function useImage(id, myClass) {
-	var elmnt = document.createElementNS(svgNS, "use");
-	elmnt.classList.add(myClass);
-	elmnt.setAttribute("href", id);
+function useImage(id, myClass0, myClass1) {
+	var elmnt = document.getElementById(id);
+	elmnt.classList.add(myClass0);
+	elmnt = document.createElementNS(svgNS, "use");
+	elmnt.classList.add(myClass1);
+	elmnt.setAttribute("href", "#"+id);
 	svg.appendChild(elmnt);
 }
