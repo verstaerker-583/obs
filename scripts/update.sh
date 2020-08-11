9e0e9e67fe2c6c0b0db00e34978d9259d7b513ed
Olaf Köpke <olaf@olafs-air.speedport.ip>
2020-08-11 22:24:39 +0200

    Initial commit

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,6 +3,7 @@
 gsed -f gpToSr.sed ../gp/okgp.json > ../sr/oksr.json
 
 gsed -f toMm.sed ../bd/okbd.json > ../scenes/mmbd.json
+gsed -f toMm.sed ../bd/okbd_naked.json > ../scenes/mmbd_naked.json
 gsed -f toMm.sed ../gp/okgp.json > ../scenes/mmgp.json
 gsed -f toMm.sed ../sr/oksr.json > ../scenes/mmsr.json
 gsed -f toMm.sed ../ap/okap.json > ../scenes/mmap.json
