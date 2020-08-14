86b6df5add7cf64177f1ae4bcf3825334975b1d9
Olaf Köpke <olaf@olafs-air.speedport.ip>
2020-08-14 21:27:44 +0200

    Initial commit

---
+++ magic/magic.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+DATE="197304291400"
+OBS="Library/Application Support/obs-studio"
+
+# Prep
+killall obs > /dev/null 2>&1
+
+sudo chown -R root `pwd` 
+sudo find `pwd` -exec touch -ht $DATE '{}' \;
+
+sudo rm -rf ~/"$OBS"
+
+rm -f ~/Library/Preferences/*obs*
+rm -f ~/Library/Preferences/*audio*
+
+sudo rm -rf /Library/Preferences/Audio
+
+osascript -e "set volume output volume 50"
+osascript -e "set volume input volume 50"
+osascript -e "set volume alert volume 0"
+
+# Profiles
+mkdir -p ~/"$OBS/basic"
+ln -sf `pwd`/profiles ~/"$OBS/basic"
+
+# Scenes
+cp -a scenes ~/"$OBS/basic"
+
+#
+cp -a global.ini ~/"$OBS"
+sudo find ~/"$OBS" -exec touch -ht $DATE '{}' \;
--- scripts/update.sh
+++ scripts/update.sh
@@ -5,25 +5,28 @@ chmod -R +w ~/Documents/GitHub/obs
 
 gsed -f gpToSr.sed ../gp/okgp.json > ../sr/oksr.json
 
-mkdir -p ../obs/scenes
-gsed -f toMm.sed ../bd/okbd.json > ../obs/scenes/mmbd.json
-gsed -f toMm.sed ../bd/okbd_naked.json > ../obs/scenes/mmbd_naked.json
-gsed -f toMm.sed ../gp/okgp.json > ../obs/scenes/mmgp.json
-gsed -f toMm.sed ../sr/oksr.json > ../obs/scenes/mmsr.json
-gsed -f toMm.sed ../ap/okap.json > ../obs/scenes/mmap.json
+mkdir -p ../magic/scenes
+gsed -f toMm.sed ../bd/okbd.json > ../magic/scenes/mmbd.json
+gsed -f toMm.sed ../bd/okbd_naked.json > ../magic/scenes/mmbd_naked.json
+gsed -f toMm.sed ../gp/okgp.json > ../magic/scenes/mmgp.json
+gsed -f toMm.sed ../sr/oksr.json > ../magic/scenes/mmsr.json
+gsed -f toMm.sed ../ap/okap.json > ../magic/scenes/mmap.json
 
 mkdir -p ../profiles/okYTlq ../profiles/okYTmq
 gsed -f lqProfiles.sed ../profiles/okRec/basic.ini > ../profiles/okYTlq/basic.ini
 gsed -f mqProfiles.sed ../profiles/okRec/basic.ini > ../profiles/okYTmq/basic.ini
 
-mkdir -p ../obs/profiles/mmRec ../obs/profiles/mmYTlq ../obs/profiles/mmYTmq
-gsed -f toMm.sed ../profiles/okRec/basic.ini	> ../obs/profiles/mmRec/basic.ini
-gsed -f toMm.sed ../profiles/okYTlq/basic.ini	> ../obs/profiles/mmYTlq/basic.ini
-gsed -f toMm.sed ../profiles/okYTmq/basic.ini	> ../obs/profiles/mmYTmq/basic.ini
+mkdir -p ../magic/profiles/mmRec ../magic/profiles/mmYTlq ../magic/profiles/mmYTmq
+gsed -f toMm.sed ../profiles/okRec/basic.ini	> ../magic/profiles/mmRec/basic.ini
+gsed -f toMm.sed ../profiles/okYTlq/basic.ini	> ../magic/profiles/mmYTlq/basic.ini
+gsed -f toMm.sed ../profiles/okYTmq/basic.ini	> ../magic/profiles/mmYTmq/basic.ini
+
+mkdir -p ../magic/profiles/gpYTlq ../magic/profiles/gpYTmq
+gsed -f toGp.sed ../profiles/okYTlq/basic.ini	> ../magic/profiles/gpYTlq/basic.ini
+gsed -f toGp.sed ../profiles/okYTmq/basic.ini	> ../magic/profiles/gpYTmq/basic.ini
 
 find ~/Documents/GitHub/obs -exec touch -ht 197304291400 '{}' \;
 
-chmod -R +w ~/Documents/GitHub/obs
+chmod -R -w ~/Documents/GitHub/obs
+chmod -R +w ~/Documents/GitHub/obs/.git
 
-#cd ../.. 
-#zip --symlinks -q -r ~/Desktop/`date "+%Y%m%d%H%M%S.zip"` obs/profiles/mm* obs/scenes/mm*.json
