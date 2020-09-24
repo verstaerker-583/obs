6099509c55f89879a9e45131816954fd476248b0
Olaf Köpke <40574338+verstaerker-583@users.noreply.github.com>
2020-09-24 07:20:49 +0200

    Initial commit

--- gp/html/data.js
+++ gp/html/data.js
@@ -1,6 +1,6 @@
 var start = {
 	theme: "#LIVE 🔴 Markus Mauthe - Naturfotograf & Umweltaktivist",
-	message_neg: "Gleich: Die Welt im Blick 📷",
+	message_neg: "Gleich: Die Welt 🌍 im Blick 📷",
 	call2action: "👁 Sei jetzt LIVE dabei!"
 };
 var finish = {
