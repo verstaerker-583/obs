0eeba0a2f3239293feebebb97417b42d7ac5cbf7
Olaf Köpke <40574338+verstaerker-583@users.noreply.github.com>
2021-03-01 00:01:02 +0100

    Update data.js

--- mm/html/data.js
+++ mm/html/data.js
@@ -1,4 +1,4 @@
 var start={theme:"Markus Mauthe Live 🔴 Naturfotograf & Umweltaktivist",message:"#AlleinKannIchDieWeltNichtRetten",call2action:"👁 Sei jetzt LIVE dabei!"};var finish={theme:start.theme,message:"Vielen Dank und auf Wiedersehen! 🌱",call2action:"👁 Sei auch nächstes Mal LIVE dabei!"};var donate={theme:"🌳 Für Aufforstung und Landkauf! 🌳",message:"https://www.amap-brazil.org/de/foerdern",call2action:"⇨ Unterstütze AMAP! 🌱"};var pics={channelIcon:"https://yt3.ggpht.com/a/AATXAJz1rf8F_1ZMrY7BkibAF0CFrkj4qja9n7fTqgrz=s188",clockLogo:"https://sofareisen.org/images/logo_schrift.png",presenterLogo:"https://yt3.ggpht.com/ytc/AAUvwnjjpmIxC7qNzEZW8axWBgX-ABZCCV6zhQpzlvC1=s70",organizationLogoNeg:"https://yt3.ggpht.com/ytc/AAUvwnhKw1dCYkoz8Zt6uXvnodOynVRFiX4fBDHzXQnJ=s70",hostLogo:"https://yt3.ggpht.com/ytc/AAUvwngniP7LA5XVERT6U-ePwCQWvcek3QREoU3GNZ-S=s70",};var alarmTime="20:00";
 
-var timezoneOffset=1;
-var videoId="vdn6JrXwir0";
+var timezoneOffset=2;
+var videoId="Ev8sRfw_xFA";
